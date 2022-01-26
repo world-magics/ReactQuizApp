ac4f67c74f9b1553e1e4eed94e7d4167e597c672
xray-ray <sherzodyarashev1997@gmail.com>
2022-01-26 23:14:19 +0500

    answer item with map

---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './AnswersList.css'
+const AnswersList=(props)=>{
+    return (
+        <div className="">
+
+        </div>
+        )
+}
+
+export default AnswersList
+
+
