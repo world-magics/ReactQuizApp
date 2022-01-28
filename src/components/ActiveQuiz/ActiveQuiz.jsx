cdc9572592ab21863836317c267e3c1f96a7b72e
Xray-ray <sherzodyarashev1997@gmail.com>
2022-01-28 14:58:27 +0300

    Question add this.state

--- src/components/ActiveQuiz/ActiveQuiz.jsx
+++ src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -6,10 +6,10 @@ const ActiveQuiz=props=>(
     <div className="ActiveQuiz">
         <p className="Question">
             <span>
-                <strong>2.</strong>&nbsp;
+                <strong>{props.answerNumber}.</strong>&nbsp;
         {props.question}
             </span>
-            <small>4 in 12</small>
+            <small>{props.answerNumber} in {props.quizLength}</small>
         </p>
        <AnswersList
        answers={props.answers}
--- src/container/Quiz/Quiz.jsx
+++ src/container/Quiz/Quiz.jsx
@@ -9,6 +9,7 @@ class Quiz extends Component{
         quiz:[
             {   question:"Math Calculate: 3+3=",
                 rightAnswerId:4,
+                id:1,
                 answers:[
                     {text:'Result: 2',id:1},
                     {text:'Result: 5',id:2},
@@ -16,7 +17,9 @@ class Quiz extends Component{
                     {text:'Result: 9',id:4},
                 ]
             },
-            {
+            {   question:"Math Calculate: 8*2=",
+                rightAnswerId:2,
+                id:2,
                 answers:[
                     {text:'Question1 1',full_text:'lorem lorem lorem lorem3'},
                     {text:'Question1 1',full_text:'lorem lorem lorem lorem3'},
@@ -30,6 +33,10 @@ class Quiz extends Component{
 
 onAnswerClickHandler=(answerId)=>{
 console.log("Answer:",answerId)
+
+this.setState({
+    activeQuestion:this.state.activeQuestion+1
+})
 }
 
     render(){
@@ -41,8 +48,8 @@ console.log("Answer:",answerId)
 <div className='QuizWrapper'>
 <h1>Answer all Question</h1>
 <ActiveQuiz
-    answers={this.state.quiz[0].answers} 
-    question={this.state.quiz[0].question}
+    answers={this.state.quiz[this.state.activeQuestion].answers} 
+    question={this.state.quiz[this.state.activeQuestion].question}
     onAnswerClick={this.onAnswerClickHandler}
     quizLength={this.state.quiz.length}
     answerNumber={this.state.activeQuestion+1}
